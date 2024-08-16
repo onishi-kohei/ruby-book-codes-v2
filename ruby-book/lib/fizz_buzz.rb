312f0ab3f3747263a93c7343d2812b1503c31bcb
onishi.kohei <kohei.o2222@gmail.com>
2024-08-16 09:17:19 +0900

    fizz_buzzのminitestによるテストコード追加

--- ruby-book/lib/fizz_buzz.rb
+++ ruby-book/lib/fizz_buzz.rb
@@ -11,7 +11,16 @@ def fizz_buzz(n)
 end
 
 
-for num in 1..15 do
-  puts fizz_buzz(num)
-end
+require 'minitest/autorun'
 
+class FizzBuzzTest < Minitest::Test
+  def test_fizz_buzz
+    assert_equal'1',fizz_buzz(1)
+    assert_equal'2',fizz_buzz(2)
+    assert_equal'Fizz',fizz_buzz(3)
+    assert_equal'4',fizz_buzz(4)
+    assert_equal'Buzz',fizz_buzz(5)
+    assert_equal'Fizz',fizz_buzz(6)
+    assert_equal'Fizz Buzz',fizz_buzz(15)
+  end
+end
