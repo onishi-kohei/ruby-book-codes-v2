26be11592ffd331a2f1d17a66ab63ffe76b004ff
onishi.kohei <kohei.o2222@gmail.com>
2024-08-20 20:46:56 +0900

    rainbowableを新規作成

---
+++ ruby-book/lib/rainbowable.rb
@@ -0,0 +1,11 @@
+module Rainbowable
+  def rainbowable
+    colored_chars =[]
+
+    to_s.each_char.map.with_index do |char, count|
+
+      color = 31 + count % 6
+      "\e[#{color}m#{char}"
+    end.join + "\e[0m"
+  end
+end
